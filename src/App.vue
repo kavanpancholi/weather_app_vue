ef260a8fdfcdae2aeef117117f9b83e92d13cd99
kavan <kavanpancholi@gmail.com>
2018-10-05 14:29:52 +0530

    Create view pages

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <b-nav>
+      <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
+      <b-nav-item :to="{ name: 'search', params:{keyword: 'test'} }">Search</b-nav-item>
+      <b-nav-item :to="{ name: 'weather', params:{woeid:'123'} }">Weather</b-nav-item>
+    </b-nav>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/WeatherWoeid.vue
@@ -0,0 +1,3 @@
+<template>
+    <weather></weather>
+</template>
